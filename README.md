d75146bd5c8d2a0d2c809907bf14b1ef924103eb
vivaxy <xyxuye2007@126.com>
2016-08-29 11:26:14 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # hook-deploy
+
 git hook to deploy
+
+# setup
+`sudo npm install pm2 -g`
+`npm run start`
+
+# update
+
+`pm2 update`
+
+# ls
+
+`pm2 ls`
+
+# kill
+
+`pm2 kill`
+
+# 
---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * @since 2016-08-29 10:46
+ * @author vivaxy
+ */
+
+const koa = require('koa');
+const child_process = require('child_process');
+
+const spawn = child_process.spawn;
+const app = koa();
+
+const PORT = 3000;
+const HOOK_DEPLOY_PATH = '/hooks/deploy';
+const HOOK_DEPLOY_SCRIPT_FILE = './script/hook-deploy.sh';
+
+const run = (done) => {
+
+    const deploy = spawn('sh', [HOOK_DEPLOY_SCRIPT_FILE]);
+
+    deploy.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    deploy.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    deploy.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        done(null, code);
+    });
+};
+
+app.use(function * () {
+
+    const request = this.request;
+    const response = this.response;
+    if (request.path === HOOK_DEPLOY_PATH) {
+
+        const code = yield run;
+
+        if (code === 0) {
+            response.status = 204;
+        } else {
+            response.status = 500;
+        }
+    }
+
+});
+
+app.listen(PORT);
