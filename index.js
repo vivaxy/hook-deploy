37f2fbbbc674c95ceb1e5a7d4e6dd41d31e4ad95
vivaxy <xyxuye2007@126.com>
2016-08-31 12:20:43 +0800

    update

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
  */
 
 const koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const child_process = require('child_process');
 
 const spawn = child_process.spawn;
 const app = koa();
 
 const PORT = 3000;
+const DATA_EVENT = 'data';
+const CLOSE_EVENT = 'close';
 const HOOK_DEPLOY_PATH = '/hooks/deploy';
 const HOOK_DEPLOY_SCRIPT_FILE = './script/hook-deploy.sh';
 
@@ -17,33 +20,40 @@ const run = (done) => {
 
     const deploy = spawn('sh', [HOOK_DEPLOY_SCRIPT_FILE]);
 
-    deploy.stdout.on('data', (data) => {
+    deploy.stdout.on(DATA_EVENT, (data) => {
         console.log(`stdout: ${data}`);
     });
 
-    deploy.stderr.on('data', (data) => {
+    deploy.stderr.on(DATA_EVENT, (data) => {
         console.log(`stderr: ${data}`);
     });
 
-    deploy.on('close', (code) => {
+    deploy.on(CLOSE_EVENT, (code) => {
         console.log(`child process exited with code ${code}`);
         done(null, code);
     });
 };
 
+app.use(bodyParser());
+
 app.use(function * () {
 
     const request = this.request;
     const response = this.response;
-    
+
     console.log(`request.path: ${request.path}`);
-    
+    console.log(`request.method: ${request.method}`);
+    console.log(`request.headers: ${JSON.stringify(request.headers)}`);
+    console.log(`request.query: ${JSON.stringify(request.query)}`);
+    console.log(`request.body: ${JSON.stringify(request.body)}`);
+
     if (request.path === HOOK_DEPLOY_PATH) {
 
         const code = yield run;
 
         if (code === 0) {
-            response.status = 204;
+            response.status = 200;
+            response.body = '{}';
         } else {
             response.status = 500;
         }
--- script/hook-deploy.sh
+++ script/hook-deploy.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+cd ..
+cd project
+git pull origin master
+npm install
+npm run build
