012c0d24dd29d995b07428a85016d41ade5fce98
vivaxy <xyxuye2007@126.com>
2016-10-08 20:31:35 +0800

    log level

--- config/log4js.js
+++ config/log4js.js
@@ -3,18 +3,19 @@
  * @author vivaxy
  */
 
-/** 
+/**
  * log levels
- ALL
- TRACE
- DEBUG
- INFO
- WARN
- ERROR
- FATAL
- OFF
+ * @see https://github.com/nomiddlename/log4js-node/blob/master/lib/levels.js#L54
+ * ALL: new Level(Number.MIN_VALUE, "ALL"),
+ * TRACE: new Level(5000, "TRACE"),
+ * DEBUG: new Level(10000, "DEBUG"),
+ * INFO: new Level(20000, "INFO"),
+ * WARN: new Level(30000, "WARN"),
+ * ERROR: new Level(40000, "ERROR"),
+ * FATAL: new Level(50000, "FATAL"),
+ * MARK: new Level(9007199254740992, "MARK"), // 2^53
+ * OFF: new Level(Number.MAX_VALUE, "OFF"),
  */
-
 module.exports = {
     appenders: [
         {
